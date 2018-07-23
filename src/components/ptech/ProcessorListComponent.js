551382c64094038879cfeda91a0c2c4011ebbc5d
Robin <robin.sumalde.ivp@gmail.com>
2018-07-23 19:45:34 +0800

    new commit
    new commit

--- src/components/ptech/ProcessorListComponent.js
+++ src/components/ptech/ProcessorListComponent.js
@@ -1,60 +1,110 @@
 import React, { Component } from 'react';
-import {Table, ProgressBar, Pagination, Modal, Button, Row , Input , Icon, Autocomplete, Col} from 'react-materialize'
+import {Table, ProgressBar, Pagination, Modal, Button, Row , Input , Icon,  Col} from 'react-materialize'
 
 export default class ProcessorListComponent extends Component {
 
-  constructor(props) {
-
+  constructor(props) 
+  {
     super(props);
-     this.state = { list: [], loading: true, newcontents:'', totalPage:''};
+     this.state = { list: [], listWithPager : [], loading: true, newcontents:'', showAll:false, _activePage:1};
 
-      this.GetToApi();
-      this.contents = '';
-      this.handleChange = this.handleChange.bind(this);
-      this.PostToApi = this.PostToApi.bind(this);
+     this.showAll = false;
+
+     
   }
 
-  handleChange(event)
+  componentDidMount(){
+    
+     this.GetAll();
+     this.GetWithPager();
+  }
+
+
+  handleChange = (event) =>
   {     
     this.setState({
       [event.target.name] : event.target.value,
     });
   }
 
-  GetToApi()
+  PageStatus = (state) => 
+  {
+     if(state.showAll)
+     {
+      this.setState({loading: false, newcontents: ProcessorListComponent.renderTable(state.list)})
+     }
+     else
+     {
+      this.setState({loading: false, newcontents: ProcessorListComponent.renderTable(state.listWithPager)})
+     }
+  }
+
+  TogglePage = (event) =>
+  { 
+     this.state.showAll = event.target.checked;
+     this.PageStatus(this.state, this.ShowAll);
+  }
+
+  GetAll = async() =>
+  {
+      fetch('http://localhost:57254/api/Processors/?ShowAll=true' , {method:'get'})
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ list: data, loading: false});  
+      });
+  }
+
+  GetWithPager = async() =>
   {
       fetch('http://localhost:57254/api/Processors' , {method:'get'})
       .then(response => response.json())
       .then(data => {
-        this.setState({ list: data, loading: false, newcontents: ProcessorListComponent.rederTable(data)});  
+        this.setState({ listWithPager: data, loading: false});  
       });
+  }
+
+  GetWithID = (event) => 
+  {
+      if(event.target.value =='')
+      {
+        this.PageStatus(this.state);
+      }
+      else
+      {
+      fetch('http://localhost:57254/api/Processors/' + event.target.value , {method:'get'})
+      .then(response => response.json())
+      .then(data => {
+        if(data.success != false)
+        {
+        this.setState({ loading: false, newcontents: ProcessorListComponent.renderTablebyID(data)});  
+        }
+      });
+    }
   }
 
   FetchPage = (pagestat) =>
   {
+      this.setState({
+        _activePage : pagestat
+      })
       fetch('http://localhost:57254/api/Processors/?CurrentPage=' + pagestat , {method:'get'})
       .then(response => response.json())
       .then(data => {
-        this.setState({ list: data, loading: false, newcontents: ProcessorListComponent.rederTable(data)});  
+        this.setState({ listWithPager: data, loading: false, newcontents: ProcessorListComponent.renderTable(data)});  
       });
   }
 
-  FetchID = (event) => {
-    fetch('http://localhost:57254/api/Processors/' + event.target.value , {method:'get'})
-    .then(response => response.json())
-    .then(data => {
-      if(data.success != false)
-      {
-      this.setState({ list: data, loading: false, newcontents: ProcessorListComponent.renderTablebyID(data)});  
-      }
-    });
-  }
-
-  async PostToApi()
+  Post = async() =>
   {
     await this.timeout(1000, fetch('http://localhost:57254/api/Processors' , {method:'POST', headers:{ 'Accept': 'application/json', 'Content-Type':'application/json'}, body:JSON.stringify(this.state)}))
-    .then(response => response.json()).catch(error => console.error('Fetch Error =\n', error));
-    this.GetToApi(); 
+    .then(response => response.json())
+    .then(data =>{
+
+    })
+    .catch(error => console.error('Fetch Error =\n', error));
+    this.GetAll();
+    this.GetWithPager();
+    this.PageStatus(this.state); 
   }
 
   async timeout(ms, promise) {
@@ -66,14 +116,9 @@ export default class ProcessorListComponent extends Component {
     })
   }
 
-  componentDidUpdate(prevProps) 
-  {
-    if (this.state.list !== prevProps.list) {
-    
-    }
-  }
+  
 
-  static rederTable(list,page) {
+  static renderTable(list,page) {
     return (      
       <div>
       <Table className="responsive-table striped highlight">
@@ -130,14 +175,14 @@ export default class ProcessorListComponent extends Component {
         <h3>Processor List</h3>       
 
         <Row>
-           <Input s={3} label="Processor ID" validate onChange={(event) => this.FetchID(event)}><Icon>search</Icon></Input> 
+           <Input s={3} name="s_processorID" label="Processor ID" validate onChange={(event) => this.GetWithID(event)}><Icon>search</Icon></Input> 
         </Row> 
 
         {contents}
         {this.state.newcontents}
 
     <Modal
-      actions={<Button onClick={this.PostToApi} className="modal-close waves-effect waves-green btn-flat">Submit<Icon left>send</Icon></Button> }
+      actions={<Button onClick={this.Post} className="modal-close waves-effect waves-green btn-flat">Submit<Icon left>send</Icon></Button> }
       header='Add Processor'
       trigger={<Button>Add Processor</Button>}>
 
@@ -147,10 +192,13 @@ export default class ProcessorListComponent extends Component {
 
     </Modal>
 
-
-   <Pagination items={this.state.list.totalPage} activePage={1} maxButtons={this.state.list.totalPage} onSelect={this.FetchPage}/>
+   <Pagination items={this.state.listWithPager.totalPage} activePage={this.state._activePage} maxButtons={this.state.listWithPager.totalPage} onSelect={this.FetchPage}/>
    
-   {this.state.activePage}
+   <center>
+      <Input id='testcheck' name='group1' type='checkbox' label='DisplayAll' onChange={(event) => this.TogglePage(event)}/>
+   </center>
+
+   <Button onClick={this.GetAll}>Get All</Button>
 
   </div>
 
